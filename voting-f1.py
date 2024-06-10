a06d23b006f6eac5cba5877d936dc0ed7db19ce0
Simon Schuster <simon.schuster20@gmail.com>
2024-06-10 16:31:33 +0200

    send DM vote reminder

--- voting-f1.py
+++ voting-f1.py
@@ -14,7 +14,7 @@ import os
 import atexit
 import signal
 
-racers = {
+RACERS = {
     "jisifus"       : "Arthur",
     "pjgangster"    : "Peter",
     "nynedine"      : "Kili",
@@ -28,6 +28,16 @@ racers = {
     "eisidrive"     : "Eisi",
     "lukas6662"     : "Ropi",
     "kleschmabilla" : "Franzi"
+}
+
+DAY_TRANSLATIONS = {
+    'Monday'   : 'Montag',
+    'Tuesday'  : 'Dienstag',
+    'Wednesday': 'Mittwoch',
+    'Thursday' : 'Donnerstag',
+    'Friday'   : 'Freitag',
+    'Saturday' : 'Samstag',
+    'Sunday'   : 'Sonntag'
 }
 
 parser = argparse.ArgumentParser(description='Discord Bot for F1 RRL Voting')
@@ -52,8 +62,7 @@ TOKEN = args.token
 MIN_NUM_RACERS = args.min_num_racers
 GUILD_ID = args.guild_id
 CHANNEL_ID = args.channel_id 
-ROLE_ID = args.role_id 
-VOTING_CLOSED_HOUR="15:00"
+ROLE_ID = args.role_id
 
 logging.info(f"min racers: {MIN_NUM_RACERS}")
 logging.info(f"token     : {TOKEN}")
@@ -62,17 +71,19 @@ logging.info(f"channel_id: {CHANNEL_ID}")
 logging.info(f"role_id   : {ROLE_ID}")
 
 
-#EMOJI_TIMESLOTS = {'🕕': '18:00', '🕖': '19:00', '🕗': '20:00'} 
-EMOJI_TIMESLOTS = {'6️⃣': '18:00', '7️⃣': '19:00', '8️⃣': '20:00'} 
+EMOJI_TIMESLOTS = {'6️⃣': '18:00', '7️⃣': '19:00', '8️⃣': '20:00'}
 EMOJI_NOT_AVAILABLE = ['👎']
-    
+VOTING_CLOSED_HOUR = 15
+VOTING_REMINDER_IMAGE_PATH = 'assets/voting_reminder.png'
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
-intents.guilds = True  
+intents.guilds = True
+intents.members = True
     
 client = discord.Client(intents=intents)
-    
+
 # Dictionary to hold message IDs and reaction counts
 message_ids = {}
 
@@ -131,8 +142,9 @@ signal.signal(signal.SIGTERM, signal_handler)  # Handle termination
 @client.event
 async def on_ready():
     logging.info(f'We have logged in as {client.user}')
-    client.loop.create_task(daily_task())
-    
+    await client.loop.create_task(daily_task())
+
+
 @client.event
 async def on_message(message):
     logging.info(f"message from {message.author} to {message.channel.id}")
@@ -140,7 +152,7 @@ async def on_message(message):
         return
     if message.channel.id != CHANNEL_ID:
         return
-    
+
     if message.content.startswith('KW '):
         logging.info(f"received {message.content} from {message.author}")
         try:
@@ -151,6 +163,8 @@ async def on_message(message):
             await message.channel.send('Invalid week number format. Please use "KW [number]".')
     elif message.content.lower() == 'start':
         await count_reactions_and_generate_charts()
+    elif message.content.lower() == 'send-reminder':
+        await send_private_message_voting_reminder()
     elif message.content.startswith('debug-store-msg-ids'):
         save_message_ids()
     elif message.content.startswith('debug-load-msg-ids'):
@@ -158,15 +172,16 @@ async def on_message(message):
         logging.info(f"loaded msg ids: {message_ids}")
 
 
-
 @client.event
 async def on_connect():
     logging.info('Bot connected')
 
+
 @client.event
 async def on_disconnect():
     logging.warning('Bot disconnected')
-    
+
+
 async def post_weekdays(channel, week_number):
     global message_ids
     message_ids.clear()
@@ -178,7 +193,7 @@ async def post_weekdays(channel, week_number):
         first_day_of_year = first_day_of_year - timedelta(first_day_of_year.weekday())
     
     start_date = first_day_of_year + timedelta(weeks=week_number-1)
-    weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
+    weekdays = list(DAY_TRANSLATIONS.values())
 
     await channel.send(f"<@&{ROLE_ID}>")
     # send info message
@@ -200,8 +215,6 @@ async def post_weekdays(channel, week_number):
     
     await channel.send(embed=embed)
 
-
-
     for i in range(7):
         day_date = start_date + timedelta(days=i)
         day_name = weekdays[day_date.weekday()]
@@ -213,65 +226,110 @@ async def daily_task():
     await client.wait_until_ready()
     while not client.is_closed():
         now = datetime.now()
-        if now.hour == 15 and now.minute == 0:
+
+        # send reminder message one hour before voting closes per pm
+        if now.hour == VOTING_CLOSED_HOUR - 1 and now.minute == 0:
+            await send_private_message_voting_reminder()
+
+        # generate daily chart with votings
+        if now.hour == VOTING_CLOSED_HOUR and now.minute == 0:
             await count_reactions_and_generate_charts()
+
         await asyncio.sleep(55)
     
 
-async def count_reactions_and_generate_charts():
+async def count_reactions_for_day(today_name_german):
     channel = client.get_channel(CHANNEL_ID)
     if channel is None:
         print(f"Failed to get channel with ID {CHANNEL_ID}")
-        return
+        return None, None, None
+
+    if today_name_german not in message_ids:
+        print(f"Message ID for {today_name_german} not found.")
+        return None, None, None
+
+    msg_id = message_ids[today_name_german]
+    try:
+        msg = await channel.fetch_message(msg_id)
+    except discord.NotFound:
+        print(f"Message ID {msg_id} not found.")
+        return None, None, None
+
+    reaction_counts = defaultdict(lambda: defaultdict(list))
+    not_available_users = set()
+    available_users = set()
+    for reaction in msg.reactions:
+        if str(reaction.emoji) in EMOJI_TIMESLOTS:
+            async for user in reaction.users():
+                if user != client.user:
+                    reaction_counts[today_name_german][EMOJI_TIMESLOTS[str(reaction.emoji)]].append(user.name)
+                    available_users.add(user.name)
+        if str(reaction.emoji) in EMOJI_NOT_AVAILABLE:
+            async for user in reaction.users():
+                if user != client.user:
+                    not_available_users.add(user.name)
+
+    return reaction_counts, not_available_users, available_users
+
+
+def get_not_voted_users(not_available_users, available_users):
+    channel = client.get_channel(CHANNEL_ID)
+    if channel is None:
+        print(f"Failed to get channel with ID {CHANNEL_ID}")
+        return []
+
+    # Get all users who have not voted yet
+    all_users = set(RACERS.keys())
+    voted_users = not_available_users.union(available_users)
+    not_voted_users = all_users.difference(voted_users)
+
+    users_to_dm = []
+    for member in channel.members:
+        if member.name in not_voted_users:
+            users_to_dm.append(member)
+
+    return users_to_dm
+
+
+def get_day_of_week():
     today_name = datetime.now().strftime('%A')
-    day_translations = {
-        'Monday': 'Montag',
-        'Tuesday': 'Dienstag',
-        'Wednesday': 'Mittwoch',
-        'Thursday': 'Donnerstag',
-        'Friday': 'Freitag',
-        'Saturday': 'Samstag',
-        'Sunday': 'Sonntag'
-    }
-    today_name_german = day_translations[today_name]
-    if today_name_german in message_ids:
-        msg_id = message_ids[today_name_german]
-        try:
-            msg = await channel.fetch_message(msg_id)
-        except discord.NotFound:
-            print(f"Message ID {msg_id} not found.")
-            return
+    return DAY_TRANSLATIONS[today_name]
 
-        reaction_counts = defaultdict(lambda: defaultdict(list))
-        not_available_users = []
-        for reaction in msg.reactions:
-            if str(reaction.emoji) in EMOJI_TIMESLOTS:
-                async for user in reaction.users():
-                    if user != client.user:
-                        reaction_counts[today_name_german][EMOJI_TIMESLOTS[str(reaction.emoji)]].append(user.name)
-            if str(reaction.emoji) in EMOJI_NOT_AVAILABLE:
-                async for user in reaction.users():
-                    if user != client.user and user not in not_available_users:
-                        not_available_users.append(user.name)
 
-        generate_barchart(today_name_german, reaction_counts, not_available_users)
+async def send_private_message_voting_reminder():
+    today_name_german = get_day_of_week()
+    reaction_counts, not_available_users, available_users = await count_reactions_for_day(today_name_german)
+    if reaction_counts is None or not_available_users is None or available_users is None:
+        logging.warning("Failed to count reactions for today.")
+        return
+
+    users = get_not_voted_users(not_available_users, available_users)
+
+    for user in users:
+        logging.info(f"Sending vote reminder to {user.name}")
+        await user.send(f"Bro scherts dich abstimmen?")
+        await user.send(file=discord.File(VOTING_REMINDER_IMAGE_PATH))
+
+
+async def count_reactions_and_generate_charts():
+    logging.info("Counting reactions and generating charts...")
+    today_name_german = get_day_of_week()
+    reaction_counts, not_available_users, available_users = await count_reactions_for_day(today_name_german)
+    if reaction_counts is None or not_available_users is None or available_users is None:
+        logging.warning("Failed to count reactions for today.")
+        return
+
+    generate_barchart(today_name_german, reaction_counts, not_available_users, available_users)
 
 
 def getUserRealName(user):
-    if user in racers:
-        return racers[user]
+    if user in RACERS:
+        return RACERS[user]
     else:
         return user
 
-def getUsersNotVoted(voted):
-    not_voted = []
-    for racer in racers.keys():
-        if racer not in voted:
-            not_voted.append(racer)
-    return not_voted
 
-
-def generate_barchart(day_name, reaction_counts, not_available_users):
+def generate_barchart(day_name, reaction_counts, not_available_users, available_users):
     logging.info(f"generate chart for {day_name}")
     logging.info(f"not available: {not_available_users}")
     timeslots = list(EMOJI_TIMESLOTS.values())
@@ -290,7 +348,6 @@ def generate_barchart(day_name, reaction_counts, not_available_users):
     draw = ImageDraw.Draw(image)
     font = ImageFont.load_default()
 
-    voted = []
     for i, timeslot in enumerate(timeslots):
         users = reaction_counts[day_name][timeslot]
         for j, user in enumerate(users):
@@ -303,13 +360,12 @@ def generate_barchart(day_name, reaction_counts, not_available_users):
                 draw.rectangle([x1, y1, x2, y2], fill=(0, 153, 0))
             else:
                 draw.rectangle([x1, y1, x2, y2], fill=(204, 0, 0))
-            voted.append(user)
             realname = getUserRealName(user)
             draw.text((x1 + 5, y1 + 10), realname, fill='white', font=font)
 
         # Draw the label for the timeslot
         draw.text((i * (bar_width + bar_spacing) + bar_spacing + 5, height - 20), timeslot, fill='black', font=font)
-    
+
     i = len(timeslots)
     draw.text((i * (bar_width + bar_spacing) + bar_spacing + 5, height - 20), "Nicht verfügbar", fill='black', font=font)
     for j, user in enumerate(not_available_users):
@@ -318,15 +374,15 @@ def generate_barchart(day_name, reaction_counts, not_available_users):
         x2 = x1 + bar_width
         y2 = y1 + block_height
         draw.rectangle([x1, y1, x2, y2], fill=(80, 80, 80))
-        voted.append(user)
         realname = getUserRealName(user)
         draw.text((x1 + 5, y1 + 10), realname, fill='white', font=font)
 
 
     i = len(timeslots) + 1
     draw.text((i * (bar_width + bar_spacing) + bar_spacing + 5, height - 20), "Nicht gevoted", fill='black', font=font)
-    not_voted = getUsersNotVoted(voted)
-    for j, user in enumerate(not_voted):
+    not_voted = get_not_voted_users(not_available_users, available_users)
+    not_voted_nicknames = [user.name for user in not_voted]
+    for j, user in enumerate(not_voted_nicknames):
         x1 = i * (bar_width + bar_spacing) + bar_spacing
         y1 = height - (j + 1) * (block_height + vertical_spacing) - 30
         x2 = x1 + bar_width
